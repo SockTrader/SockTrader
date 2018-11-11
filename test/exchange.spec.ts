c249dabec5bea9787f3bdc06d3c1a090d6a22d94
Wouter Cleymans <woutercleymans@gmail.com>
2018-11-11 18:02:58 +0100

    improve tests

--- test/exchange.spec.ts
+++ test/exchange.spec.ts
@@ -6,7 +6,8 @@ import Exchange, {ITradeablePair} from "../src/core/exchanges/exchange";
 import {OrderSide} from "../src/core/orderInterface";
 import Orderbook from "../src/core/orderbook";
 import CandleCollection from "../src/core/candleCollection";
-import {client} from "websocket";
+import {EventEmitter} from "events";
+import {connection} from "websocket";
 
 const pair = 'BTCETH';
 
@@ -18,40 +19,40 @@ class MockExchange extends Exchange {
 }
 
 describe('Exchange', () => {
+    let exc = new MockExchange();
+
+    beforeEach(() => {
+        exc = new MockExchange();
+    });
 
     it('Should generate a random order id', () => {
-        const exc = new MockExchange();
         const orderId = exc['generateOrderId'](pair);
         expect(orderId).to.be.a('string');
         expect(orderId).to.have.lengthOf(32);
     });
 
     it('Should create a buy order', () => {
-        const exc = new MockExchange();
-        const createOrder = spy(exc, <any>"createOrder");
+        const createOrder = spy(exc, "createOrder" as any);
         exc.buy(pair, 1, 10);
         expect(createOrder.calledOnce).to.eq(true);
         expect(createOrder.args[0]).to.deep.equal(["BTCETH", 1, 10, "buy"]);
     });
 
     it('Should create a sell order', () => {
-        const exc = new MockExchange();
-        const createOrder = spy(exc, <any>"createOrder");
+        const createOrder = spy(exc, "createOrder" as any);
         exc.sell(pair, 1, 10);
         expect(createOrder.calledOnce).to.eq(true);
         expect(createOrder.args[0]).to.deep.equal(["BTCETH", 1, 10, "sell"]);
     });
 
     it('Should put an order into progress when creating an order', () => {
-        const exc = new MockExchange();
-        const setOrderInProgress = spy(exc, <any>"setOrderInProgress");
+        const setOrderInProgress = spy(exc, "setOrderInProgress" as any);
         const orderId = exc["createOrder"](pair, 1, 10, OrderSide.SELL);
         expect(setOrderInProgress.calledOnce).to.eq(true);
         expect(exc["orderInProgress"][orderId]).to.equal(true);
     });
 
     it('Should put an order in/out of progress', () => {
-        const exc = new MockExchange();
         const id = 'ORDER_123';
         exc["setOrderInProgress"](id, true);
         expect(exc["orderInProgress"][id]).to.equal(true);
@@ -59,8 +60,7 @@ describe('Exchange', () => {
         expect(exc["orderInProgress"][id]).to.equal(undefined);
     });
 
-    it('Should return an orderbook for a trading pair', () => {
-        const exc = new MockExchange();
+    it('Should return a cached orderbook for a trading pair', () => {
         exc["currencies"] = [{tickSize: 0.001, id: pair} as ITradeablePair];
         const ob = exc.getOrderbook(pair);
         expect(ob).to.be.an.instanceof(Orderbook);
@@ -70,8 +70,7 @@ describe('Exchange', () => {
         expect(ob).to.be.equal(ob2);
     });
 
-    it('Should return a candle collection for a trading pair', () => {
-        const exc = new MockExchange();
+    it('Should return a cached candle collection for a trading pair', () => {
         const interval = {code: "M1", cron: "00 */1 * * * *"};
         const ob = exc.getCandleCollection(pair, interval, () => {
         });
@@ -84,9 +83,8 @@ describe('Exchange', () => {
     });
 
     it('Should track all order changes', () => {
-        const exc = new MockExchange();
-        const addOrder = spy(exc, <any>"addOrder");
-        const removeOrder = spy(exc, <any>"removeOrder");
+        const addOrder = spy(exc, "addOrder" as any);
+        const removeOrder = spy(exc, "removeOrder" as any);
         exc.onReport({params: {reportType: "new", clientOrderId: '123'}});
         expect(addOrder.calledOnce).to.eq(true);
 
@@ -108,7 +106,6 @@ describe('Exchange', () => {
     });
 
     it("Should connect via a websocket connection string", () => {
-        const exc = new MockExchange();
         const spyOn = spy(exc["socketClient"], "on");
         const spyConnect = spy(exc["socketClient"], "connect");
 
@@ -117,5 +114,29 @@ describe('Exchange', () => {
         expect(spyOn.args[0][0]).to.equal("connectFailed");
         expect(spyOn.args[1][0]).to.equal("connect");
         expect(spyConnect.args[0]).to.deep.equal(['wss://my.fake.socket']);
+    });
+
+    it('Should remove all event listeners once the exchange is destroyed', () => {
+        // This test should prevent memory leaks in an exchange.
+        const spyRemoveListeners = spy(exc, "removeAllListeners");
+
+        exc.destroy();
+
+        expect(exc).to.be.an.instanceof(EventEmitter);
+        expect(spyRemoveListeners.calledOnce).to.eq(true);
+    });
+
+    it('Should send messages over a socket connection', () => {
+        expect(() => exc.send('test_method', {param1: 'param1', param2: 'param2'}))
+            .to.throw("First connect to the exchange before sending instructions..");
+
+        const connection = {send: () => null};
+        const mockConnection = mock(connection);
+        exc["connection"] = connection as any;
+        const result = JSON.stringify({"method": "test", "params": {"param1": "1", "param2": "2"}, "id": "test"});
+        mockConnection.expects("send").once().withArgs(result);
+
+        exc.send('test', {param1: '1', param2: '2'});
+        mockConnection.verify();
     });
 });
