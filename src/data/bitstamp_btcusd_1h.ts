cec0e1ce6db9673ffb538fb506f9c0dd8393f218
Wouter Cleymans <woutercleymans@gmail.com>
2019-03-23 09:45:15 +0100

    Fix wrong merge

--- src/data/bitstamp_btcusd_1h.ts
+++ src/data/bitstamp_btcusd_1h.ts
@@ -6,21 +6,22 @@ import CandleLoader from "../sockTrader/core/candles/candleLoader";
 const SRC_PATH = "../../src/data";
 const PATH = path.resolve(__dirname, SRC_PATH, "bitstamp_btcusd_1h.csv");
 
-const parser = (dataFrame: IDataFrame<number, any>): IDataFrame<number, any> => {
-    return dataFrame
-        .dropSeries(["Symbol"])
-        .renameSeries({
-            "Date": "timestamp",
-            "High": "high",
-            "Low": "low",
-            "Open": "open",
-            "Close": "close",
-            "Volume To": "volume",
-        })
-        .select(row => {
-            row.timestamp = moment(row.timestamp, "YYYY-MM-DD hh-A");
-            return row;
-        });
-};
-
-export default new CandleLoader(PATH, parser);
+// noinspection JSUnusedGlobalSymbols
+export default new CandleLoader(PATH, {decimalSeperator: ".", symbol: ["BTC", "USD"], name: "Bitcoin"},
+    (dataFrame: IDataFrame<number, any>): IDataFrame<number, any> => {
+        return dataFrame
+            .dropSeries(["Symbol"])
+            .renameSeries({
+                "Date": "timestamp",
+                "High": "high",
+                "Low": "low",
+                "Open": "open",
+                "Close": "close",
+                "Volume From": "volume",
+            })
+            .select(row => {
+                row.timestamp = moment(row.timestamp, "YYYY-MM-DD hh-A");
+                return row;
+            });
+    },
+);
