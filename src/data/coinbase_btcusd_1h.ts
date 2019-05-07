434629a5e92ad476684ade638345f943f9356041
Wouter Cleymans <woutercleymans@gmail.com>
2019-05-07 07:05:45 +0200

    #13 Downloaded new crypto data from cryptodatadownload.com

--- src/data/coinbase_btcusd_1h.ts
+++ src/data/coinbase_btcusd_1h.ts
@@ -5,20 +5,20 @@ import {ICandle} from "../sockTrader/core/candleCollection";
 import CandleNormalizer from "../sockTrader/data/candleNormalizer";
 
 const SRC_PATH = "../../src/data";
-const PATH = path.resolve(__dirname, SRC_PATH, "bitstamp_btcusd_1h.csv");
+const PATH = path.resolve(__dirname, SRC_PATH, "coinbase_btcusd_1h.csv");
 
 // noinspection JSUnusedGlobalSymbols
 export default new CandleNormalizer(PATH, {symbol: ["BTC", "USD"], name: "Bitcoin"},
     (dataFrame: IDataFrame<number, any>): IDataFrame<number, ICandle> => {
         return dataFrame
-            .dropSeries(["Symbol"])
+            .dropSeries(["Symbol", "Volume BTC"])
             .renameSeries({
                 "Date": "timestamp",
                 "High": "high",
                 "Low": "low",
                 "Open": "open",
                 "Close": "close",
-                "Volume From": "volume",
+                "Volume USD": "volume",
             })
             .select(row => {
                 row.timestamp = moment(row.timestamp, "YYYY-MM-DD hh-A");
