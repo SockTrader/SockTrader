baf2734650077d80f9835dbd80dbacea0d83c49d
Wouter Cleymans <woutercleymans@gmail.com>
2019-08-23 22:18:42 +0200

    Fix merge issues

--- src/__tests__/core/exchanges/hitBTC.spec.ts
+++ src/__tests__/core/exchanges/hitBTC.spec.ts
@@ -1,216 +1,190 @@
-import moment from "moment";
-import {EventEmitter} from "events";
 import {Pair} from "../../../sockTrader/core/types/pair";
 import HitBTC, {CandleInterval} from "../../../sockTrader/core/exchanges/hitBTC";
-import {IOrder, OrderSide} from "../../../sockTrader/core/types/order";
-import Orderbook from "../../../sockTrader/core/orderbook";
-import {IOrderbookData} from "../../../sockTrader/core/types/IOrderbookData";
-import {ICandle} from "../../../sockTrader/core/types/ICandle";
 import WebSocket from "../../../sockTrader/core/connection/webSocket";
+import HitBTCCommand from "../../../sockTrader/core/exchanges/commands/hitBTCCommand";
+import HitBTCCandleProcessor from "../../../sockTrader/core/exchanges/candleProcessors/hitBTCCandleProcessor";
+import HitBTCOrderCreator from "../../../sockTrader/core/exchanges/orderCreators/hitBTCOrderCreator";
 
 jest.mock("./../../../config");
+jest.mock("./../../../sockTrader/core/connection/webSocket");
+
+process.env.SOCKTRADER_TRADING_MODE = "LIVE";
 
 const pair: Pair = ["BTC", "USD"];
 
-function createExchange() {
-    const exchange = new HitBTC();
-    exchange.send = jest.fn();
-
-    return exchange;
-}
-
-let exchange = createExchange();
+let exchange = new HitBTC();
 beforeEach(() => {
-    exchange = createExchange();
+    (WebSocket as any).mockClear();
+    exchange = new HitBTC();
 });
 
-describe("subscribeReports", () => {
-    test("Should send out a subscribe to report events", () => {
-        exchange.subscribeReports();
-        expect(exchange.send).toBeCalledWith(expect.objectContaining({
-            id: "subscribeReports",
-            method: "subscribeReports",
-            params: {},
-        }));
-    });
-});
-
-describe("subscribeOrderbook", () => {
-    test("Should send out a subscribe to orderbook events", () => {
-        exchange.subscribeOrderbook(pair);
-        expect(exchange.send).toBeCalledWith(
-            expect.objectContaining({
-                id: "subscribeOrderbook",
-                method: "subscribeOrderbook",
-                params: {symbol: "BTCUSD"},
-            }),
-        );
-    });
-});
-
-describe("subscribeCandles", () => {
-    test("Should send out subscribe to candle events", () => {
-        exchange.subscribeCandles(pair, CandleInterval.FIVE_MINUTES);
-        expect(exchange.send).toBeCalledWith(
-            expect.objectContaining({
-                id: "subscribeCandles",
-                method: "subscribeCandles",
-                params: {period: "M5", symbol: "BTCUSD"},
-            }),
-        );
-    });
-});
-
-describe("login", () => {
-    test("Should authenticate user on exchange", () => {
-        exchange.login("PUB_123", "PRIV_123");
-        expect(exchange.send).toBeCalledWith(
-            expect.objectContaining({
-                id: "login",
-                method: "login",
-                params: {
-                    algo: "HS256",
-                    pKey: "PUB_123",
-                    signature: expect.any(String),
-                    nonce: expect.any(String),
-                },
-            }),
-        );
-    });
-});
-
-describe("onSnapshotCandles", () => {
-    test("Should set candles in candle manager when snapshot has been received", () => {
-        const set = jest.fn();
-        exchange.getCandleManager = jest.fn(() => ({set})) as any;
-
-        const candles: ICandle[] = [{close: 1, high: 2, low: 0, open: 0, timestamp: moment(), volume: 10} as ICandle];
-
-        exchange.onSnapshotCandles(pair, candles, CandleInterval.FIVE_MINUTES);
-        expect(set).toBeCalled();
-    });
-});
-
-describe("onUpdateOrderbook", () => {
-    function createOrderbook() {
-        const orderbook = new Orderbook(["BTC", "USD"], 6);
-        orderbook.setOrders = jest.fn();
-        orderbook.addIncrement = jest.fn();
-
-        return orderbook;
-    }
-
-    test("Should update an in memory orderbook", () => {
-        const orderbook = createOrderbook();
-        const getOrderbook = jest.fn(() => orderbook);
-
-        exchange.getOrderbook = getOrderbook;
-        exchange.emit = jest.fn();
-
-        const ob: IOrderbookData = {
-            sequence: 1,
-            pair,
-            ask: [{price: 0.0015, size: 100}],
-            bid: [{price: 0.001391, size: 40}],
-        };
-
-        exchange.onUpdateOrderbook(ob);
-        expect(getOrderbook).toBeCalled();
-        expect(orderbook.addIncrement).toBeCalledWith([{price: 0.0015, size: 100}], [{price: 0.001391, size: 40}], 1);
-        expect(exchange.emit).toBeCalledWith("core.updateOrderbook", expect.objectContaining({
-            pair: ["BTC", "USD"],
-            precision: 6,
-        }));
+describe("createConnection", () => {
+    it("Should create a new websocket connection", () => {
+        const connection = exchange["createConnection"]();
+        expect(connection).toBeInstanceOf(WebSocket);
     });
 });
 
 describe("loadCurrencies", () => {
     it("Should load currency configuration from the exchange", () => {
         exchange.loadCurrencies();
-        expect(exchange.send).toBeCalledWith(expect.objectContaining({
-            id: "getSymbols",
+
+        const arg1 = (exchange["connection"].send as any).mock.calls[0][0] as HitBTCCommand;
+        expect(arg1).toBeInstanceOf(HitBTCCommand);
+        expect(arg1).toEqual({
+            restorable: false,
             method: "getSymbols",
             params: {},
-        }));
+        });
     });
 });
 
-describe("cancelOrder", () => {
-    it("Should cancel an order", () => {
-        exchange["orderManager"]["setOrderProcessing"] = jest.fn();
-        exchange.cancelOrder({id: "123"} as IOrder);
+describe("login", () => {
+    test("Should authenticate user on exchange", () => {
+        exchange.login("PUB_123", "PRIV_123");
 
-        expect(exchange["orderManager"]["setOrderProcessing"]).toBeCalledWith("123");
-        expect(exchange.send).toBeCalledWith(expect.objectContaining({
-            id: "cancelOrder",
-            method: "cancelOrder",
+        const arg1 = (exchange["connection"].send as any).mock.calls[0][0] as HitBTCCommand;
+        expect(arg1).toBeInstanceOf(HitBTCCommand);
+        expect(arg1).toEqual({
+            restorable: true,
+            method: "login",
             params: {
-                clientOrderId: "123",
+                algo: "HS256",
+                pKey: "PUB_123",
+                signature: expect.any(String),
+                nonce: expect.any(String),
             },
-        }));
+        });
     });
 });
 
-describe("adjustOrder", () => {
-    it("Should adjust existing orders", () => {
-        exchange["isAdjustingAllowed"] = jest.fn(() => true);
+describe("orderbook management", () => {
+    beforeEach(() => {
+        exchange.currencies = {BTCUSD: {id: ["BTC", "USD"], quantityIncrement: 0.001, tickSize: 0.000001}};
+        exchange.onSnapshotOrderbook({
+            sequence: 1,
+            pair,
+            ask: [{price: 0.0015, size: 100}],
+            bid: [{price: 0.001391, size: 40}],
+        });
+    });
 
-        exchange.adjustOrder({pair: pair, id: "123"} as IOrder, 0.002, 0.5);
-        expect(exchange.send).toBeCalledWith(expect.objectContaining({
-            id: "cancelReplaceOrder",
-            method: "cancelReplaceOrder",
-            params: {
-                clientOrderId: "123",
-                price: 0.002,
-                quantity: 0.5,
-                strictValidate: true,
-                requestClientId: expect.any(String),
-            },
-        }));
+    describe("onSnapshotOrderbook", () => {
+        test("Should emit incremental changes", () => {
+            const emitSpy = jest.spyOn(exchange, "emit");
+            exchange.onSnapshotOrderbook({
+                sequence: 2,
+                pair,
+                ask: [{price: 0.0025, size: 100}],
+                bid: [{price: 0.002391, size: 40}],
+            });
+
+            expect(emitSpy).toBeCalledWith("core.snapshotOrderbook", {
+                sequenceId: 2,
+                precision: 6,
+                pair: ["BTC", "USD"],
+                ask: [{price: 0.0025, size: 100}],
+                bid: [{price: 0.002391, size: 40}],
+            });
+        });
+    });
+
+    describe("onUpdateOrderbook", () => {
+        test("Should emit incremental changes", () => {
+            const emitSpy = jest.spyOn(exchange, "emit");
+
+            exchange.onUpdateOrderbook({
+                sequence: 2,
+                pair,
+                ask: [{price: 0.0015, size: 0}, {price: 0.0016, size: 10}],
+                bid: [{price: 0.001391, size: 50}],
+            });
+
+            expect(emitSpy).toBeCalledWith("core.updateOrderbook", {
+                sequenceId: 2,
+                precision: 6,
+                pair: ["BTC", "USD"],
+                ask: [{price: 0.0016, size: 10}],
+                bid: [{price: 0.001391, size: 50}],
+            });
+        });
     });
 });
 
-describe("createOrder", () => {
-    it("Should create a new order", () => {
-        exchange["createOrder"](pair, 10, 1, OrderSide.BUY);
+describe("subscribeCandles", () => {
+    test("Should send out subscribe to candle events", () => {
+        exchange.subscribeCandles(pair, CandleInterval.FIVE_MINUTES);
 
-        expect(exchange.send).toBeCalledWith(expect.objectContaining({
-            id: "newOrder",
-            method: "newOrder",
-            params: {
-                price: 10,
-                quantity: 1,
-                side: "buy",
-                symbol: pair,
-                type: "limit",
-                clientOrderId: expect.any(String),
-            },
-        }));
+        const arg1 = (exchange["connection"].send as any).mock.calls[0][0] as HitBTCCommand;
+        expect(arg1).toBeInstanceOf(HitBTCCommand);
+        expect(arg1).toEqual({
+            restorable: true,
+            method: "subscribeCandles",
+            params: {period: "M5", symbol: "BTCUSD"},
+        });
+    });
+});
+
+describe("subscribeOrderbook", () => {
+    test("Should send out a subscribe to orderbook events", () => {
+        exchange.subscribeOrderbook(pair);
+
+        const arg1 = (exchange["connection"].send as any).mock.calls[0][0] as HitBTCCommand;
+        expect(arg1).toBeInstanceOf(HitBTCCommand);
+        expect(arg1).toEqual({
+            restorable: true,
+            method: "subscribeOrderbook",
+            params: {symbol: "BTCUSD"},
+        });
+    });
+});
+
+describe("subscribeReports", () => {
+    test("Should send out a subscribe to report events", () => {
+        exchange.subscribeReports();
+
+        const arg1 = (exchange["connection"].send as any).mock.calls[0][0] as HitBTCCommand;
+        expect(arg1).toBeInstanceOf(HitBTCCommand);
+        expect(arg1).toEqual({
+            restorable: true,
+            method: "subscribeReports",
+            params: {},
+        });
     });
 });
 
 describe("onConnect", () => {
     test("Should initialize when exchange is connected", () => {
-        exchange.loadCurrencies = jest.fn();
         exchange.login = jest.fn();
-        exchange.adapter.onReceive = jest.fn();
+        exchange.loadCurrencies = jest.fn();
+
+        const onSpy = jest.spyOn(exchange["connection"], "on");
+        const onReceiveSpy = jest.spyOn(exchange["adapter"], "onReceive");
 
         exchange["onConnect"]();
-        // @ts-ignore
-        exchange.getConnection = jest.fn(() => new EventEmitter());
 
-        const message = JSON.stringify({test: "123"});
-        exchange["connection"].emit("message", message);
+        const [arg1, arg2] = onSpy.mock.calls[0];
+        expect(arg1).toEqual("message");
+        expect(arg2).toBeInstanceOf(Function);
 
-        expect(exchange.adapter.onReceive).toBeCalledWith(message);
+        arg2(JSON.stringify({test: 123}));
+
+        expect(onReceiveSpy).toBeCalledWith(JSON.stringify({test: 123}));
         expect(exchange.loadCurrencies).toBeCalledTimes(1);
         expect(exchange.login).toBeCalledWith("pub_key", "sec_key");
     });
 });
 
-describe("createConnection", () => {
-    it("Should create a new websocket connection", () => {
-        const connection = exchange["createConnection"]();
-        expect(connection).toBeInstanceOf(WebSocket)
+describe("getCandleProcessor", () => {
+    test("Should return a new candle processor instance", () => {
+        const instance = exchange["getCandleProcessor"]();
+        expect(instance).toBeInstanceOf(HitBTCCandleProcessor);
+    });
+});
+
+describe("getOrderCreator", () => {
+    test("Should return a new order creator instance", () => {
+        const instance = exchange["getOrderCreator"]();
+        expect(instance).toBeInstanceOf(HitBTCOrderCreator);
     });
 });
