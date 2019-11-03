3d11d9f6be64bb0d04c167676fb00ac94a0e6251
Wouter Cleymans <woutercleymans@gmail.com>
2019-11-03 20:34:57 +0100

    Log profit and average buy price

--- src/sockTrader/core/__mocks__/logger.ts
+++ src/sockTrader/core/__mocks__/logger.ts
@@ -1,6 +1,13 @@
-export default {
+const logger = {
     debug: jest.fn(),
     error: jest.fn(),
     info: jest.fn(),
     input: jest.fn(),
 };
+
+export const orderbookLogger = {...logger};
+export const walletLogger = {...logger};
+export const candleLogger = {...logger};
+export const orderLogger = {...logger};
+
+export default {...logger};
--- src/sockTrader/core/plugins/tradeProfitCalculator.ts
+++ src/sockTrader/core/plugins/tradeProfitCalculator.ts
@@ -1,3 +1,4 @@
+import {orderLogger} from "../logger";
 import {IOrder, OrderSide, OrderStatus} from "../types/order";
 import {IReportAware} from "../types/plugins/IReportAware";
 
@@ -14,18 +15,20 @@ export default class TradeProfitCalculator implements IReportAware {
         if ([OrderStatus.FILLED, OrderStatus.PARTIALLY_FILLED].indexOf(status) < 0) return;
 
         if (side === OrderSide.SELL) {
+            if (quantity > this.remainingAssets) return;
             this.remainingAssets -= quantity;
+
+            const net = price - this.avgBuyPrice;
+            const perc = net / this.avgBuyPrice;
+
+            orderLogger.info(`Profit: ${net} ${perc}`);
         }
 
         if (side === OrderSide.BUY) {
             this.avgBuyPrice = this.getWeightedAverage(quantity, price);
             this.remainingAssets += quantity;
+
+            orderLogger.info(`Avg buy: ${this.avgBuyPrice}`);
         }
-
-        const avgBuyPrice = this.avgBuyPrice;
-        const percentage = (price - this.avgBuyPrice) / this.avgBuyPrice;
-
-        console.log(side, avgBuyPrice, percentage);
     }
-
 }
