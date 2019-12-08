b07bd075c1ceb82815a72693fdc77813dab53977
Wouter Cleymans <woutercleymans@gmail.com>
2019-12-08 21:11:14 +0100

    Improve unit testing

---
+++ src/__tests__/core/plugins/logging/orderLogger.spec.ts
@@ -0,0 +1,14 @@
+import logger from "../../../../sockTrader/core/logger";
+import OrderLogger from "../../../../sockTrader/core/plugins/logging/orderLogger";
+import {FX_NEW_BUY_ORDER} from "../../../../__fixtures__/order";
+
+jest.mock("../../../../sockTrader/core/logger");
+
+describe("onReport", () => {
+    test("Should log order reports", () => {
+        const orderLogger = new OrderLogger();
+        orderLogger.onReport(FX_NEW_BUY_ORDER);
+
+        expect(logger.info).toBeCalledWith("Order: {\"side\":\"buy\",\"quantity\":1,\"price\":100,\"status\":\"new\"}");
+    });
+});
---
+++ src/__tests__/core/plugins/logging/spreadLogger.spec.ts
@@ -0,0 +1,30 @@
+import logger from "../../../../sockTrader/core/logger";
+import SpreadLogger from "../../../../sockTrader/core/plugins/logging/spreadLogger";
+import Orderbook from "../../../../sockTrader/core/orderbook";
+import {FX_ASK, FX_BID} from "../../../../__fixtures__/orderbook";
+
+jest.mock("../../../../sockTrader/core/logger");
+
+function createOrderbook() {
+    const orderbook = new Orderbook(["BTC", "USD"]);
+    orderbook.setOrders(FX_ASK, FX_BID, 1);
+
+    return orderbook;
+}
+
+describe("onUpdateOrderbook", () => {
+    test("Should log orderbook spread", () => {
+        const orderLogger = new SpreadLogger();
+        orderLogger.onUpdateOrderbook(createOrderbook());
+
+        expect(logger.info).toBeCalledWith("-0.0016945986336464561 BID: 0.074944 ASK: 0.074817");
+    });
+
+    test("Should not log if spread did not change", () => {
+        const orderLogger = new SpreadLogger();
+        orderLogger.onUpdateOrderbook(createOrderbook());
+        orderLogger.onUpdateOrderbook(createOrderbook());
+
+        expect(logger.info).toBeCalledTimes(1);
+    });
+});
