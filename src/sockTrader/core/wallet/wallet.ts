6cf62d188511864bbc7d02e409ab4f3de9004642
Wouter Cleymans <woutercleymans@gmail.com>
2020-01-26 19:56:09 +0100

    #34: split reserve and release asset in separate classes

--- src/sockTrader/core/wallet/wallet.ts
+++ src/sockTrader/core/wallet/wallet.ts
@@ -1,8 +1,9 @@
 import Events from "../events";
 import {Order, OrderSide, OrderStatus, ReportType} from "../types/order";
-import {Pair} from "../types/pair";
-import {AssetCalculator, AssetMap, OrderSideCalculator, OrderSideCalculators} from "../types/wallet";
+import {AssetMap} from "../types/wallet";
 import {AssetCollection} from "./assetCollection";
+import {ReleaseCommand} from "./command/releaseCommand";
+import {ReserveCommand} from "./command/reserveCommand";
 
 /**
  * The wallet keeps track of all assets
@@ -25,50 +26,6 @@ export default class Wallet {
         return order.side === OrderSide.BUY ? this.assets.isBuyAllowed(order) : this.assets.isSellAllowed(order);
     }
 
-    private createOrderSideCalculator(orderSide: OrderSide, side: OrderSide): OrderSideCalculator {
-        return (asset: string, calc: AssetCalculator, priceQty: number) => {
-            if (orderSide === side) calc(asset, priceQty);
-        };
-    }
-
-    private createCalculators(order: Order): OrderSideCalculators {
-        return {
-            ifBuy: this.createOrderSideCalculator(order.side, OrderSide.BUY),
-            ifSell: this.createOrderSideCalculator(order.side, OrderSide.SELL),
-        };
-    }
-
-    /**
-     * The reservation of assets will be reverted in case an order has been canceled / replaced.
-     */
-    private revertAssetReservation(price: number, quantity: number, [quote, base]: Pair, {ifBuy, ifSell}: OrderSideCalculators): void {
-        ifBuy(base, this.assets.addAsset, price * quantity);
-        ifSell(quote, this.assets.addAsset, quantity);
-        ifBuy(base, this.reservedAssets.subtractAsset, price * quantity);
-        ifSell(quote, this.reservedAssets.subtractAsset, quantity);
-    }
-
-    /**
-     * Reserve assets. This will prevent a trader from spending the same amount twice.
-     * Ofc the exchange would throw an error at some point.
-     */
-    private reserveAsset(price: number, quantity: number, [quote, base]: Pair, {ifBuy, ifSell}: OrderSideCalculators): void {
-        ifBuy(base, this.assets.subtractAsset, price * quantity);
-        ifSell(quote, this.assets.subtractAsset, quantity);
-        ifBuy(base, this.reservedAssets.addAsset, price * quantity);
-        ifSell(quote, this.reservedAssets.addAsset, quantity);
-    }
-
-    /**
-     * The trade is considered to be filled and assets will be released on the other side.
-     */
-    private releaseAsset(price: number, quantity: number, [quote, base]: Pair, {ifBuy, ifSell}: OrderSideCalculators): void {
-        ifBuy(quote, this.assets.addAsset, quantity);
-        ifSell(base, this.assets.addAsset, price * quantity);
-        ifBuy(base, this.reservedAssets.subtractAsset, price * quantity);
-        ifSell(quote, this.reservedAssets.subtractAsset, quantity);
-    }
-
     private emitUpdate() {
         return Events.emit("core.updateAssets", this.assets.getAssets(), this.reservedAssets.getAssets());
     }
@@ -79,21 +36,19 @@ export default class Wallet {
      * @param {Order} oldOrder old order
      */
     updateAssets(order: Order, oldOrder?: Order) {
-        const calculators = this.createCalculators(order);
-
         if (ReportType.REPLACED === order.reportType && oldOrder) {
-            this.revertAssetReservation(oldOrder.price, oldOrder.quantity, oldOrder.pair, this.createCalculators(oldOrder));
-            this.reserveAsset(order.price, order.quantity, order.pair, calculators);
+            new ReserveCommand(this.assets, this.reservedAssets).revert(oldOrder);
+            new ReserveCommand(this.assets, this.reservedAssets).apply(order);
             this.emitUpdate();
         } else if (ReportType.NEW === order.reportType) {
-            this.reserveAsset(order.price, order.quantity, order.pair, calculators);
+            new ReserveCommand(this.assets, this.reservedAssets).apply(order);
             this.emitUpdate();
         } else if (ReportType.TRADE === order.reportType && OrderStatus.FILLED === order.status) {
             // @TODO what if order is partially filled?
-            this.releaseAsset(order.price, order.quantity, order.pair, calculators);
+            new ReleaseCommand(this.assets, this.reservedAssets).apply(order);
             this.emitUpdate();
         } else if ([ReportType.CANCELED, ReportType.EXPIRED, ReportType.SUSPENDED].indexOf(order.reportType) > -1) {
-            this.revertAssetReservation(order.price, order.quantity, order.pair, calculators);
+            new ReserveCommand(this.assets, this.reservedAssets).revert(order);
             this.emitUpdate();
         }
     }
