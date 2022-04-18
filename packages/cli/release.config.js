ddabd8274e9a79769f58cb88e73a02b2bd24bfc8
Wouter Cleymans <woutercleymans@gmail.com>
2022-04-18 17:53:30 +0200

    fix(ci): trigger bump

--- packages/cli/release.config.js
+++ packages/cli/release.config.js
@@ -8,6 +8,15 @@ module.exports = {
     extends: `${pathToRepoRoot}/release.config.base.js`,
     pkgRoot: `${pathToRepoRoot}/dist/${srcRoot}`,
     tagFormat: name + '-v${version}',
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/npm", {
+            "pkgRoot": `${pathToRepoRoot}/dist/${srcRoot}`,
+            // "pkgRoot": "dist",
+        }],
+        '@semantic-release/github'
+    ],
     commitPaths: [
         // REMOVING external dependencies to decrease unneeded releases and bad change logs
         // `${pathToRepoRoot}/workspace.json`,
--- release.config.base.js
+++ release.config.base.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 
 module.exports = {
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
-        '@semantic-release/github'
-    ],
-    branches: [
-        '+([0-9])?(.{+([0-9]),x}).x',
-        'main',
-        'next',
-        {name: 'beta', prerelease: true},
-        {name: 'alpha', prerelease: true},
-    ],
+    // plugins: [
+    //     '@semantic-release/commit-analyzer',
+    //     '@semantic-release/release-notes-generator',
+    //     '@semantic-release/npm',
+    //     '@semantic-release/github'
+    // ],
+    // branches: [
+    //     '+([0-9])?(.{+([0-9]),x}).x',
+    //     'main',
+    //     'next',
+    //     {name: 'beta', prerelease: true},
+    //     {name: 'alpha', prerelease: true},
+    // ],
 };
