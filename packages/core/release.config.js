d1f897a9c7882e5d79988388f77365dc4601c2eb
Wouter Cleymans <woutercleymans@gmail.com>
2022-04-18 15:05:16 +0200

    fix(ci): semantic release in monorepo

---
+++ packages/core/release.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+const name = 'core';
+const srcRoot = `packages/${name}`;
+const pathToRepoRoot = '../..';
+
+module.exports = {
+    extends: `${pathToRepoRoot}/release.config.base.js`,
+    pkgRoot: `${pathToRepoRoot}/dist/${srcRoot}`,
+    tagFormat: name + '-v${version}',
+    commitPaths: [
+        // REMOVING external dependencies to decrease unneeded releases and bad change logs
+        // `${pathToRepoRoot}/workspace.json`,
+        // `${pathToRepoRoot}/nx.json`,
+        // `${pathToRepoRoot}/.nxignore`,
+        // `${pathToRepoRoot}/package.json`,
+        // `${pathToRepoRoot}/.prettierrc`,
+        // `${pathToRepoRoot}/.prettierignore`,
+        `*`, // anything in this directory
+    ],
+};
---
+++ release.config.base.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+module.exports = {
+    // branches: [
+    //   '+([0-9])?(.{+([0-9]),x}).x',
+    //   'main',
+    //   'next',
+    //   'next-major',
+    //   { name: 'beta', prerelease: true },
+    //   { name: 'alpha', prerelease: true },
+    // ],
+};
