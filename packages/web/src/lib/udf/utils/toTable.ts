019726072073498567016d6ad631037a0d7a272c
Wouter Cleymans <woutercleymans@gmail.com>
2023-07-26 21:31:47 +0200

    Chore: nx upgrade

--- packages/web/src/lib/udf/utils/toTable.ts
+++ packages/web/src/lib/udf/utils/toTable.ts
@@ -1,18 +1,18 @@
-export type Table<U> = { [K in keyof U]: U[K][] }
+export type Table<U> = { [K in keyof U]: U[K][] };
 
-export const toTable = <T>(list: T[]): Table<T> => {
+export const toTable = <T extends object>(list: T[]): Table<T> => {
   return list.reduce((prev, current) => {
-    const keys = Object.keys(current) as (keyof T)[]
-    const result: Table<T> = { ...prev }
+    const keys = Object.keys(current) as (keyof T)[];
+    const result: Table<T> = { ...prev };
 
     keys.forEach((k) => {
       if (!Array.isArray(result[k])) {
-        result[k] = []
+        result[k] = <Table<T>[keyof T]>[];
       }
 
-      (result[k] as T[typeof k][]).push(current[k])
-    })
+      (<Table<T>[keyof T]>result[k]).push(current[k]);
+    });
 
-    return result
-  }, {} as Table<T>)
-}
+    return result;
+  }, <Table<T>>{});
+};
