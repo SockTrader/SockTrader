0949195550d7db7df0799642be7560887ac662c1
Wouter Cleymans <woutercleymans@gmail.com>
2023-08-06 14:37:39 +0200

    Chore: prettier cli package

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
-import { Command } from 'commander'
-import { version } from '../package.json'
-import { run } from './lib/commands/run'
-import { web } from './lib/commands/web'
+import { Command } from 'commander';
+import { version } from '../package.json';
+import { run } from './lib/commands/run';
+import { web } from './lib/commands/web';
 
-const program = new Command('socktrader')
+const program = new Command('socktrader');
 
-program
-  .version(version)
-  .addCommand(run)
-  .addCommand(web)
+program.version(version).addCommand(run).addCommand(web);
 
-program.parse(process.argv)
-
+program.parse(process.argv);
