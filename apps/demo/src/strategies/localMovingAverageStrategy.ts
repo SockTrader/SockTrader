4380bed99b49c7a50e36889f9b68a26ee48bd887
Wouter Cleymans <woutercleymans@gmail.com>
2023-08-06 14:37:48 +0200

    Chore: prettier demo app

--- apps/demo/src/strategies/localMovingAverageStrategy.ts
+++ apps/demo/src/strategies/localMovingAverageStrategy.ts
@@ -1,7 +1,7 @@
-import parse from 'date-fns/parse'
-import { CrossDown, CrossUp, SMA } from 'technicalindicators'
-import * as data from '../../../../data/coinbase_btcusd_1h.json'
-import { Candle, LocalExchange, Order, OrderSide, OrderStatus, OrderType, Strategy } from '@socktrader/core'
+import parse from 'date-fns/parse';
+import { CrossDown, CrossUp, SMA } from 'technicalindicators';
+import * as data from '../../../../data/coinbase_btcusd_1h.json';
+import { Candle, LocalExchange, Order, OrderSide, OrderStatus, OrderType, Strategy } from '@socktrader/core';
 
 interface CoinbaseCandle {
   Date: string;
@@ -15,66 +15,67 @@ interface CoinbaseCandle {
 }
 
 export class LocalMovingAverageStrategy implements Strategy {
+  private readonly _localExchange: LocalExchange;
 
-  private readonly _localExchange: LocalExchange
+  private canBuy = true;
 
-  private canBuy = true
+  private canSell = false;
 
-  private canSell = false
+  private fastSMA: SMA;
 
-  private fastSMA: SMA
+  private slowSMA: SMA;
 
-  private slowSMA: SMA
+  private crossUp: CrossUp;
 
-  private crossUp: CrossUp
-
-  private crossDown: CrossDown
+  private crossDown: CrossDown;
 
   constructor() {
-    this.fastSMA = new SMA({ period: 12, values: [] })
-    this.slowSMA = new SMA({ period: 24, values: [] })
-    this.crossUp = new CrossUp({ lineA: [], lineB: [] })
-    this.crossDown = new CrossDown({ lineA: [], lineB: [] })
+    this.fastSMA = new SMA({ period: 12, values: [] });
+    this.slowSMA = new SMA({ period: 24, values: [] });
+    this.crossUp = new CrossUp({ lineA: [], lineB: [] });
+    this.crossDown = new CrossDown({ lineA: [], lineB: [] });
 
-    this._localExchange = new LocalExchange()
-    this._localExchange.addCandles(['BTC', 'USDT'], data.candles.map((c: CoinbaseCandle) => this.mapCoinbaseCandle(c)))
-    this._localExchange.setAssets([{
-      asset: 'USDT',
-      quantity: 10000
-    }])
+    this._localExchange = new LocalExchange();
+    this._localExchange.addCandles(
+      ['BTC', 'USDT'],
+      data.candles.map((c: CoinbaseCandle) => this.mapCoinbaseCandle(c))
+    );
+    this._localExchange.setAssets([
+      {
+        asset: 'USDT',
+        quantity: 10000,
+      },
+    ]);
   }
 
   onStart(): void {
-    this._localExchange
-      .candles('BTCUSDT')
-      .subscribe(candle => this.updateCandle(candle))
+    this._localExchange.candles('BTCUSDT').subscribe((candle) => this.updateCandle(candle));
 
-    this._localExchange.orders$
-      .subscribe(order => this.updateOrder(order))
+    this._localExchange.orders$.subscribe((order) => this.updateOrder(order));
   }
 
   updateOrder(order: Order): void {
     if (order.status === OrderStatus.FILLED) {
-      this.canBuy = order.side === OrderSide.SELL
-      this.canSell = order.side === OrderSide.BUY
+      this.canBuy = order.side === OrderSide.SELL;
+      this.canSell = order.side === OrderSide.BUY;
     }
   }
 
   updateCandle(candle: Candle): void {
-    const fastSMA = this.fastSMA.nextValue(candle.close)
-    const slowSMA = this.slowSMA.nextValue(candle.close)
+    const fastSMA = this.fastSMA.nextValue(candle.close);
+    const slowSMA = this.slowSMA.nextValue(candle.close);
 
-    const up = fastSMA != null ? this.crossUp.nextValue(fastSMA, slowSMA ?? 0) : false
-    const down = fastSMA != null ? this.crossDown.nextValue(fastSMA, slowSMA ?? 0) : false
+    const up = fastSMA != null ? this.crossUp.nextValue(fastSMA, slowSMA ?? 0) : false;
+    const down = fastSMA != null ? this.crossDown.nextValue(fastSMA, slowSMA ?? 0) : false;
 
     if (up && this.canBuy) {
-      this.canBuy = false
-      return this.buy(0.1, candle.close)
+      this.canBuy = false;
+      return this.buy(0.1, candle.close);
     }
 
     if (down && this.canSell) {
-      this.canSell = false
-      return this.sell(0.1, candle.close)
+      this.canSell = false;
+      return this.sell(0.1, candle.close);
     }
   }
 
@@ -83,8 +84,8 @@ export class LocalMovingAverageStrategy implements Strategy {
       symbol: 'BTCUSDT',
       price: price,
       type: OrderType.LIMIT,
-      quantity: quantity
-    })
+      quantity: quantity,
+    });
   }
 
   sell(quantity: number, price: number): void {
@@ -92,8 +93,8 @@ export class LocalMovingAverageStrategy implements Strategy {
       symbol: 'BTCUSDT',
       price: price,
       type: OrderType.LIMIT,
-      quantity: quantity
-    })
+      quantity: quantity,
+    });
   }
 
   mapCoinbaseCandle(candle: CoinbaseCandle) {
@@ -104,7 +105,6 @@ export class LocalMovingAverageStrategy implements Strategy {
       close: candle.Close,
       volume: candle['Volume BTC'],
       start: parse(candle.Date, 'yyyy-MM-dd hh-a', new Date()),
-    }
+    };
   }
-
 }
--- apps/demo/src/strategies/movingAverageStrategy.ts
+++ apps/demo/src/strategies/movingAverageStrategy.ts
@@ -1,63 +1,59 @@
-import { Binance, Candle, Order, OrderSide, OrderStatus, OrderType, Strategy } from '@socktrader/core'
-import { CandleChartInterval } from 'binance-api-node'
-import { CrossDown, CrossUp, SMA } from 'technicalindicators'
+import { Binance, Candle, Order, OrderSide, OrderStatus, OrderType, Strategy } from '@socktrader/core';
+import { CandleChartInterval } from 'binance-api-node';
+import { CrossDown, CrossUp, SMA } from 'technicalindicators';
 
 export class MovingAverageStrategy implements Strategy {
+  private readonly _binance: Binance;
 
-  private readonly _binance: Binance
+  private canBuy = true;
 
-  private canBuy = true
+  private canSell = false;
 
-  private canSell = false
+  private fastSMA: SMA;
 
-  private fastSMA: SMA
+  private slowSMA: SMA;
 
-  private slowSMA: SMA
+  private crossUp: CrossUp;
 
-  private crossUp: CrossUp
-
-  private crossDown: CrossDown
+  private crossDown: CrossDown;
 
   constructor() {
-    this.fastSMA = new SMA({ period: 12, values: [] })
-    this.slowSMA = new SMA({ period: 24, values: [] })
-    this.crossUp = new CrossUp({ lineA: [], lineB: [] })
-    this.crossDown = new CrossDown({ lineA: [], lineB: [] })
+    this.fastSMA = new SMA({ period: 12, values: [] });
+    this.slowSMA = new SMA({ period: 24, values: [] });
+    this.crossUp = new CrossUp({ lineA: [], lineB: [] });
+    this.crossDown = new CrossDown({ lineA: [], lineB: [] });
 
-    this._binance = new Binance()
+    this._binance = new Binance();
   }
 
   onStart(): void {
-    this._binance
-      .candles({ symbol: 'BTCUSDT', interval: CandleChartInterval.ONE_HOUR, })
-      .subscribe(candle => this.updateCandle(candle))
+    this._binance.candles({ symbol: 'BTCUSDT', interval: CandleChartInterval.ONE_HOUR }).subscribe((candle) => this.updateCandle(candle));
 
-    this._binance.orders$
-      .subscribe(order => this.updateOrder(order))
+    this._binance.orders$.subscribe((order) => this.updateOrder(order));
   }
 
   updateOrder(order: Order): void {
     if (order.status === OrderStatus.FILLED) {
-      this.canBuy = order.side === OrderSide.SELL
-      this.canSell = order.side === OrderSide.BUY
+      this.canBuy = order.side === OrderSide.SELL;
+      this.canSell = order.side === OrderSide.BUY;
     }
   }
 
   updateCandle(candle: Candle): void {
-    const fastSMA = this.fastSMA.nextValue(candle.close)
-    const slowSMA = this.slowSMA.nextValue(candle.close)
+    const fastSMA = this.fastSMA.nextValue(candle.close);
+    const slowSMA = this.slowSMA.nextValue(candle.close);
 
-    const up = fastSMA != null ? this.crossUp.nextValue(fastSMA, slowSMA ?? 0) : false
-    const down = fastSMA != null ? this.crossDown.nextValue(fastSMA, slowSMA ?? 0) : false
+    const up = fastSMA != null ? this.crossUp.nextValue(fastSMA, slowSMA ?? 0) : false;
+    const down = fastSMA != null ? this.crossDown.nextValue(fastSMA, slowSMA ?? 0) : false;
 
     if (up && this.canBuy) {
-      this.canBuy = false
-      return this.buy(1, candle.close)
+      this.canBuy = false;
+      return this.buy(1, candle.close);
     }
 
     if (down && this.canSell) {
-      this.canSell = false
-      return this.sell(1, candle.close)
+      this.canSell = false;
+      return this.sell(1, candle.close);
     }
   }
 
@@ -66,8 +62,8 @@ export class MovingAverageStrategy implements Strategy {
       symbol: 'BTCUSDT',
       price: price,
       type: OrderType.LIMIT,
-      quantity: quantity
-    })
+      quantity: quantity,
+    });
   }
 
   sell(quantity: number, price: number): void {
@@ -75,8 +71,7 @@ export class MovingAverageStrategy implements Strategy {
       symbol: 'BTCUSDT',
       price: price,
       type: OrderType.LIMIT,
-      quantity: quantity
-    })
+      quantity: quantity,
+    });
   }
-
 }
