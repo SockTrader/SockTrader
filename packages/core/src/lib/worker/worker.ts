28f2eb597072a4e2b4597759cece9d9deb2eefb7
Wouter Cleymans <woutercleymans@gmail.com>
2023-08-06 14:37:29 +0200

    Chore: prettier core package

--- packages/core/src/lib/worker/worker.ts
+++ packages/core/src/lib/worker/worker.ts
@@ -1,44 +1,42 @@
-import { SHARE_ENV, Worker as NativeWorker } from 'worker_threads'
-import { WorkerData } from './worker.interfaces'
+import { SHARE_ENV, Worker as NativeWorker } from 'worker_threads';
+import { WorkerData } from './worker.interfaces';
 
 export class Worker {
-
-  private _tsNodeAvailable: boolean | undefined
+  private _tsNodeAvailable: boolean | undefined;
 
   run(script: string): void {
     new NativeWorker(this._createScript(), {
       eval: true,
       workerData: <WorkerData>{
-        strategy: script
+        strategy: script,
       },
-      env: SHARE_ENV
-    })
+      env: SHARE_ENV,
+    });
   }
 
   private _createScript(): string {
-    if (!this.isTsNodeAvailable()) throw new Error('ts-node is not installed. Run "npm install -D ts-node“ and continue.')
+    if (!this.isTsNodeAvailable()) throw new Error('ts-node is not installed. Run "npm install -D ts-node“ and continue.');
 
     return `
       require("ts-node/register/transpile-only");
       require("${__dirname}/execution.script").default();
-    `
+    `;
   }
 
   private isTsNodeAvailable(): boolean {
-    if (this._tsNodeAvailable) return this._tsNodeAvailable
+    if (this._tsNodeAvailable) return this._tsNodeAvailable;
 
     try {
-      require.resolve('ts-node')
-      this._tsNodeAvailable = true
+      require.resolve('ts-node');
+      this._tsNodeAvailable = true;
     } catch (e: unknown) {
       if (e && (e as { code?: string })?.code === 'MODULE_NOT_FOUND') {
-        this._tsNodeAvailable = false
+        this._tsNodeAvailable = false;
       } else {
-        throw e
+        throw e;
       }
     }
 
-    return this._tsNodeAvailable
+    return this._tsNodeAvailable;
   }
-
 }
